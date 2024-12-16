b572128c21ce8c8cb13266121262f663aedb4691
Guilherme Henrique <henrique.guilhermexv@gmail.com>
2024-12-16 18:55:28 -0300

    Desafio controle de fluxo

---
+++ DesafioControleDeFluxo/src/exception/ParametrosInvalidosException.java
@@ -0,0 +1,11 @@
+package src.exception;
+
+public class ParametrosInvalidosException extends RuntimeException {
+
+    public ParametrosInvalidosException() { }
+
+    public ParametrosInvalidosException(String message) {
+        super(message);
+    }
+
+}
