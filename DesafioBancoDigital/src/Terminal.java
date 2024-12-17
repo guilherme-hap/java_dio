8efe8a3a6d75875d03efc64f36bea661aa8f69cb
Guilherme Henrique <henrique.guilhermexv@gmail.com>
2024-12-17 14:32:11 -0300

    Desafio banco digital

---
+++ DesafioBancoDigital/src/Terminal.java
@@ -0,0 +1,19 @@
+package src;
+
+public class Terminal {
+
+    public static void main(String[] args) {
+        Cliente guilherme = new Cliente();
+        guilherme.setNome("Guilherme");
+
+        Conta cc = new ContaCorrente(guilherme);
+        Conta poupanca = new ContaPoupanca(guilherme);
+
+        cc.depositar(100);
+        cc.transferir(100, poupanca);
+
+        cc.imprimirExtrato();
+        poupanca.imprimirExtrato();
+    }
+
+}
